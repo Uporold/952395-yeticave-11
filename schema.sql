6cf27859a44215a13bbfabb0e4079984416a1d9f
Uporold <crubick1337@gmail.com>
2019-11-02 03:08:33 +0300

    Создаём схему БД личного проекта

--- schema.sql
+++ schema.sql
@@ -17,13 +17,20 @@ CREATE TABLE lots (
   path     VARCHAR(128) UNIQUE,
   st_price INT NOT NULL,
   dt_end   TIMESTAMP NOT NULL,
-  bet_step INT NOT NULL
+  bet_step INT NOT NULL,
+
+  autor_id INT NOT NULL,
+  winner_id INT NOT NULL,
+  cat_id INT NOT NULL
 );
 
 CREATE TABLE bets (
   id       INT AUTO_INCREMENT PRIMARY KEY,
   dt_add   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  value    INT NOT NULL
+  value    INT NOT NULL,
+
+  user_id  INT NOT NULL,
+  lot_id   INT NOT NULL
 );
 
 CREATE TABLE users (
@@ -32,7 +39,11 @@ CREATE TABLE users (
   email    VARCHAR(128) NOT NULL UNIQUE,
   name     VARCHAR(64) NOT NULL,
   password VARCHAR(64) NOT NULL,
-  contacts VARCHAR(128) NOT NULL
+  contacts VARCHAR(128) NOT NULL,
+
+  lot_id   INT,
+  bet_id   INT
+
 );
 
 CREATE UNIQUE INDEX name ON lots(name);
