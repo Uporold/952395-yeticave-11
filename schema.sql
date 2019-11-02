5de2acc47cbe7a73032a1f9dc484b0be0d339436
Uporold <crubick1337@gmail.com>
2019-11-02 20:34:40 +0300

    Создаём схему БД личного проекта

--- schema.sql
+++ schema.sql
@@ -5,30 +5,28 @@ CREATE DATABASE yeticave
 
 CREATE TABLE categories (
   id       INT AUTO_INCREMENT PRIMARY KEY,
-  name     VARCHAR(64) NOT NULL,
+  cat_name VARCHAR(64) NOT NULL,
   code     VARCHAR(64) NOT NULL
 );
 
 CREATE TABLE lots (
   id       INT AUTO_INCREMENT PRIMARY KEY,
-  dt_add   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  name     VARCHAR(64) UNIQUE NOT NULL,
-  text     VARCHAR(255),
-  path     VARCHAR(128) UNIQUE,
+  lot_name VARCHAR(64) NOT NULL,
+  cat_id   VARCHAR(64) NOT NULL,
   st_price INT NOT NULL,
+  path     VARCHAR(128) UNIQUE,
+  dt_add   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   dt_end   TIMESTAMP NOT NULL,
-  bet_step INT NOT NULL,
-
+  text     VARCHAR(255),
+  bet_step INT,
   autor_id INT NOT NULL,
-  winner_id INT NOT NULL,
-  cat_id INT NOT NULL
+  winner_id INT
 );
 
 CREATE TABLE bets (
   id       INT AUTO_INCREMENT PRIMARY KEY,
   dt_add   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   value    INT NOT NULL,
-
   user_id  INT NOT NULL,
   lot_id   INT NOT NULL
 );
@@ -36,20 +34,18 @@ CREATE TABLE bets (
 CREATE TABLE users (
   id       INT AUTO_INCREMENT PRIMARY KEY,
   dt_reg   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  email    VARCHAR(128) NOT NULL UNIQUE,
+  email    VARCHAR(128) NOT NULL,
   name     VARCHAR(64) NOT NULL,
   password VARCHAR(64) NOT NULL,
   contacts VARCHAR(128) NOT NULL,
-
   lot_id   INT,
   bet_id   INT
-
 );
 
-CREATE UNIQUE INDEX name ON lots(name);
-CREATE UNIQUE INDEX path ON lots(path);
+CREATE UNIQUE INDEX lot_name ON lots(lot_name);
 CREATE UNIQUE INDEX email ON users(email);
-CREATE INDEX user_name ON users(name);
-CREATE INDEX lot_descr ON lots(text);
+CREATE INDEX name ON users(name);
+CREATE INDEX cat_name ON categories(cat_name);
+CREATE INDEX value ON bets(value);
 CREATE INDEX start_price ON lots(st_price);
 
