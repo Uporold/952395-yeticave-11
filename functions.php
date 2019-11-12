0eea3b2df2c512d7f39c4a0247c59c1089a6c3c7
Uporold <crubick1337@gmail.com>
2019-11-12 07:53:13 +0300

    Получаем данные из формы

--- functions.php
+++ functions.php
@@ -62,3 +62,112 @@ function timeExp(string $date) {
     return $expInfo;
 }
 
+/**
+ * Создает подготовленное выражение на основе готового SQL запроса и переданных данных
+ *
+ * @param $link mysqli Ресурс соединения
+ * @param $sql string SQL запрос с плейсхолдерами вместо значений
+ * @param array $data Данные для вставки на место плейсхолдеров
+ *
+ * @return mysqli_stmt Подготовленное выражение
+ */
+function db_get_prepare_stmt($link, $sql, $data = []) {
+    $stmt = mysqli_prepare($link, $sql);
+
+    if ($stmt === false) {
+        $errorMsg = 'Не удалось инициализировать подготовленное выражение: ' . mysqli_error($link);
+        die($errorMsg);
+    }
+
+    if ($data) {
+        $types = '';
+        $stmt_data = [];
+
+        foreach ($data as $value) {
+            $type = 's';
+
+            if (is_int($value)) {
+                $type = 'i';
+            }
+            else if (is_string($value)) {
+                $type = 's';
+            }
+            else if (is_double($value)) {
+                $type = 'd';
+            }
+
+            if ($type) {
+                $types .= $type;
+                $stmt_data[] = $value;
+            }
+        }
+
+        $values = array_merge([$stmt, $types], $stmt_data);
+
+        $func = 'mysqli_stmt_bind_param';
+        $func(...$values);
+
+        if (mysqli_errno($link) > 0) {
+            $errorMsg = 'Не удалось связать подготовленное выражение с параметрами: ' . mysqli_error($link);
+            die($errorMsg);
+        }
+    }
+
+    return $stmt;
+}
+
+function validateCategory($id, $allowed_list) {
+    if (!in_array($id, $allowed_list)) {
+        return "Указана несуществующая категория";
+    }
+
+    return null;
+}
+
+function validateLength($value, $min, $max) {
+    if ($value) {
+        $len = strlen($value);
+        if ($len < $min or $len > $max) {
+            return "Значение должно быть от $min до $max символов";
+        }
+    }
+
+    return null;
+}
+
+function validateNumber($value) {
+    if ($value > 0) {
+        return null;
+    }
+    else {
+        return "Значение должно быть больше нуля!";
+    }
+}
+function is_date_valid(string $date) : bool {
+    $format_to_check = 'Y-m-d';
+    $dateTimeObj = date_create_from_format($format_to_check, $date);
+
+    return $dateTimeObj !== false && array_sum(date_get_last_errors()) === 0;
+}
+
+function getPostVal($name) {
+    return filter_input(INPUT_POST, $name);
+}
+
+function dateCheck($date) {
+    date_default_timezone_set("Europe/Moscow");
+    setlocale(LC_ALL, 'ru_RU');
+ if (is_date_valid($date) == 1) {
+    $expDate = strtotime($date);
+    $secs_to_expire = $expDate - time();
+    if ($secs_to_expire >= 84000) {
+        return null;
+    }
+    else {
+        return "Дата окончания торгов не может быть менее суток!";
+    }
+ }
+ else {
+     return "Неверный формат даты";
+ }
+}
---
+++ templates/_add.php
@@ -0,0 +1,76 @@
+<nav class="nav">
+      <ul class="nav__list container">
+      <?php foreach ($categories as $category): ?>
+        <li class="nav__item">
+            <a href="pages/all-lots.html"><?=$category['cat_name'];?></a>
+        </li>
+        <?php endforeach; ?>
+      </ul>
+    </nav>
+    <?php $classname = isset($errors) ? "form--invalid" : ""; ?>
+    <form class="form form--add-lot container <?= $classname; ?>" action="add.php" method="post" enctype="multipart/form-data"> <!-- form--invalid -->
+      <h2>Добавление лота</h2>
+      <div class="form__container-two">
+      <?php $classname = isset($errors['lot_name']) ? "form__item--invalid" : ""; ?>
+        <div class="form__item <?= $classname; ?>"> <!-- form__item--invalid -->
+          <label for="lot-name">Наименование <sup>*</sup></label>
+          <input id="lot-name" type="text" name="lot_name" placeholder="Введите наименование лота" value="<?= getPostVal('lot_name'); ?>">
+          <span class="form__error">Введите наименование лота</span>
+        </div>
+        <?php $classname = isset($errors['cat_id']) ? "form__item--invalid" : ""; ?>
+        <div class="form__item <?= $classname; ?>">
+          <label for="category">Категория <sup>*</sup></label>
+          <select  name="cat_id" id="category">
+            <option>Выберите категорию</option>
+            <?php foreach ($categories as $category): ?>
+                <option value="<?=$category['id'] ?>"
+                <?php if ($category['id'] == getPostVal('cat_id')): ?>selected<?php endif; ?>><?=$category['cat_name']; ?>
+                </option>
+            <?php endforeach; ?>
+          </select>
+          <span class="form__error">Выберите категорию</span>
+        </div>
+      </div>
+      <?php $classname = isset($errors['text']) ? "form__item--invalid" : ""; ?>
+      <div class="form__item form__item--wide <?= $classname; ?>">
+        <label for="text">Описание <sup>*</sup></label>
+        <textarea id="text" name="text" placeholder="Напишите описание лота"><?= getPostVal('text'); ?></textarea>
+        <span class="form__error">Напишите описание лота</span>
+      </div>
+      <div class="form__item form__item--file">
+        <label for="lot_img">Изображение <sup>*</sup></label>
+        <div class="form__input-file">
+          <input class="visually-hidden" type="file" name="lot_img" id="lot_img" value="">
+          <label for="lot_img">
+            Добавить
+          </label>
+        </div>
+      </div>
+      <div class="form__container-three">
+      <?php $classname = isset($errors['st_price']) ? "form__item--invalid" : ""; ?>
+        <div class="form__item form__item--small <?= $classname; ?>">
+          <label for="st_price">Начальная цена <sup>*</sup></label>
+          <input id="st_price" type="text" name="st_price" placeholder="0" value="<?= getPostVal('st_price'); ?>">
+          <span class="form__error">Введите начальную цену</span>
+        </div>
+        <?php $classname = isset($errors['bet_step']) ? "form__item--invalid" : ""; ?>
+        <div class="form__item form__item--small <?= $classname; ?>">
+          <label for="bet_step">Шаг ставки <sup>*</sup></label>
+          <input id="bet_step" type="text" name="bet_step" placeholder="0" value="<?= getPostVal('bet_step'); ?>">
+          <span class="form__error">Введите шаг ставки</span>
+        </div>
+        <?php $classname = isset($errors['dt_end']) ? "form__item--invalid" : ""; ?>
+        <div class="form__item <?= $classname; ?>">
+          <label for="dt_end">Дата окончания торгов <sup>*</sup></label>
+          <input class="form__input-date" id="dt_end" type="text" name="dt_end" placeholder="Введите дату в формате ГГГГ-ММ-ДД" value="<?= getPostVal('dt_end'); ?>">
+          <span class="form__error">Введите дату завершения торгов</span>
+        </div>
+      </div>
+      <?php if (isset($errors)): ?>
+      <span class="form__error form__error--bottom">Пожалуйста, исправьте следующие ошибки:
+          <?php foreach ($errors as $val): ?>
+                <li><strong><?= $val; ?></strong></li>
+             <?php endforeach; ?></span>
+      <?php endif; ?>
+      <button type="submit" class="button">Добавить лот</button>
+    </form>
