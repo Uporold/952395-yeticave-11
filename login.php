0e17a3f9f0dd8000b0d36296b30d990baac6c117
Uporold <crubick1337@gmail.com>
2019-11-18 06:13:51 +0300

    Аутентификация

---
+++ login.php
@@ -0,0 +1,75 @@
+<?php
+require_once 'init.php';
+require_once 'functions.php';
+$container = 0;
+
+$sql = 'SELECT `id`, `cat_name`, `code` FROM categories';
+$res = mysqli_query($con, $sql);
+if ($res) {
+    $categories = mysqli_fetch_all($res, MYSQLI_ASSOC);
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $form = $_POST;
+    $errors = [];
+    $required = ['email', 'password'];
+
+
+    foreach ($required as $field) {
+	    if (empty($form[$field])) {
+	        $errors[$field] = 'Это поле надо заполнить';
+        }
+    }
+    $errors = array_filter($errors);
+
+	$email = mysqli_real_escape_string($con, $form['email']);
+	$sql = "SELECT * FROM users WHERE email = '$email'";
+	$res = mysqli_query($con, $sql);
+
+	$user = $res ? mysqli_fetch_array($res, MYSQLI_ASSOC) : null;
+
+	if (!count($errors) and $user) {
+		if (password_verify($form['password'], $user['password'])) {
+			$_SESSION['user'] = $user;
+        }
+
+		else {
+			$errors['password'] = 'Неверный пароль';
+        }
+
+    }
+    elseif (empty($form['email'])) {
+        $errors['email'] = 'Введите email';
+    }
+    elseif ($form['email'] === $user['email']) {
+        $errors['email'] = null;
+    }
+    else {
+		$errors['email'] = 'Такой пользователь не найден';
+	}
+
+	if (count($errors)) {
+		$page_content = include_template('_login.php', ['form' => $form, 'errors' => $errors, 'categories' => $categories]);
+	}
+	else {
+		header("Location: /index.php");
+		exit();
+	}
+}
+else {
+    $page_content = include_template('_login.php', ['categories' => $categories]);
+
+    if (isset($_SESSION['user'])) {
+        header("Location: /index.php");
+        exit();
+    }
+}
+
+$layout_content = include_template('layout.php', [
+	'content'    => $page_content,
+    'categories' => $categories,
+    'container' => $container,
+	'title'      => 'Yeticave - вход'
+]);
+
+echo $layout_content;
--- templates/layout.php
+++ templates/layout.php
@@ -28,19 +28,19 @@ $container;
         <a class="main-header__add-lot button" href="add.php">Добавить лот</a>
 
         <nav class="user-menu">
-            <?php if ($is_auth === 1): ?>
+            <?php if  (isset($_SESSION['user'])):  ?>
                 <div class="user-menu__logged">
-                    <p><?= $user_name; ?></p>
+                    <p><?=strip_tags($_SESSION['user']['name']); ?></p>
                     <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
-                    <a class="user-menu__logout" href="#">Выход</a>
+                    <a class="user-menu__logout" href="logout.php">Выход</a>
                 </div>
             <?php else: ?>
                 <ul class="user-menu__list">
                     <li class="user-menu__item">
-                        <a href="#">Регистрация</a>
+                        <a href="reg.php">Регистрация</a>
                     </li>
                     <li class="user-menu__item">
-                        <a href="#">Вход</a>
+                        <a href="login.php">Вход</a>
                     </li>
                 </ul>
             <?php endif; ?>
