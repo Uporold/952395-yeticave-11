fb07d8bae0963edd38c81cf27c68584702c1294f
Uporold <crubick1337@gmail.com>
2019-11-20 22:07:15 +0300

    Полнотекстовый поиск

--- category.php
+++ category.php
@@ -16,17 +16,18 @@ $q = filter_input(INPUT_GET, 'q');
     if ($q == $_GET['q']) {
         $sort_field = $_GET['q'];
     }
-    $sql = 'SELECT lots.id, lot_name, st_price, path, dt_end, categories.cat_name  FROM lots '
-    . 'JOIN categories ON categories.id = lots.cat_id '
-    . 'WHERE categories.code ="' . $sort_field . '"'
-    . 'ORDER BY dt_add DESC LIMIT 9';
     $cur_page = $_GET['page'] ?? 1;
     $page_items = 1;
-    $result = mysqli_query($con, $sql);
-    $items_count = mysqli_num_rows($result);
+    $result = mysqli_query($con, 'SELECT COUNT(*) as cnt FROM lots '
+    . 'JOIN categories ON categories.id = lots.cat_id '
+    . 'WHERE categories.code ="' . $sort_field . '"'
+    . 'ORDER BY dt_add DESC LIMIT 9');
+    $items_count = mysqli_fetch_assoc($result)['cnt'];
     $pages_count = ceil($items_count / $page_items);
     $offset = ($cur_page - 1) * $page_items;
+    var_dump($result);
     $pages = range(1, $pages_count);
+
 
     $sql = 'SELECT lots.id, lot_name, st_price, path, dt_end, categories.cat_name  FROM lots '
     . 'JOIN categories ON categories.id = lots.cat_id '
@@ -35,10 +36,7 @@ $q = filter_input(INPUT_GET, 'q');
 
     if ($res = mysqli_query($con, $sql)) {
         $lots = mysqli_fetch_all($res, MYSQLI_ASSOC);
-       /* $page_content = include_template('_category.php', ['lots' => $lots, 'categories' => $categories, 'pages' => $pages,
-    'pages_count' => $pages_count,
-    'cur_page' => $cur_page, 'link' => $link]);*/
-    $page_content = include_template('_category.php', compact('lots', 'categories', 'pages', 'pages_count', 'cur_page', 'link'));
+        $page_content = include_template('_category.php', compact('lots', 'categories', 'pages', 'pages_count', 'cur_page', 'link'));
     } else {
         $page_content = include_template('error.php', ['error' => mysqli_error($con)]);
     }
--- search.php
+++ search.php
@@ -16,45 +16,34 @@ $link = '/search.php';
     $search = $_GET['q'] ?? '';
 
     if ($search) {
-        $sql = "SELECT lots.id, lot_name, cat_id, cat_name, st_price, path, dt_add, dt_end, text, bet_step FROM lots "
-          . "JOIN categories ON lots.cat_id  = categories.id "
-          . "WHERE MATCH(lot_name, text) AGAINST(?)";
         $cur_page = $_GET['page'] ?? 1;
         $page_items = 1;
-        $stmt = db_get_prepare_stmt($con, $sql, [$search]);
-        mysqli_stmt_execute($stmt);
-        $result = mysqli_stmt_get_result($stmt);
-        $items_count = mysqli_num_rows($result);
+        $result = mysqli_query($con, "SELECT COUNT(*) as cnt FROM lots "
+        . "JOIN categories ON lots.cat_id  = categories.id "
+        . "WHERE MATCH(lot_name, text) AGAINST( '". $search ."')");
+
+        $items_count = mysqli_fetch_assoc($result)['cnt'];
         $pages_count = ceil($items_count / $page_items);
         $offset = ($cur_page - 1) * $page_items;
-
+        var_dump($result);
         $pages = range(1, $pages_count);
+
         $sql = "SELECT lots.id, lot_name, cat_id, cat_name, st_price, path, dt_add, dt_end, text, bet_step FROM lots "
           . "JOIN categories ON lots.cat_id  = categories.id "
           . "WHERE MATCH(lot_name, text) AGAINST(?)"
           . "ORDER BY dt_add DESC LIMIT " . $page_items . " OFFSET " . $offset;
-
         $stmt = db_get_prepare_stmt($con, $sql, [$search]);
         mysqli_stmt_execute($stmt);
         $result = mysqli_stmt_get_result($stmt);
-
 
         $lots = mysqli_fetch_all($result, MYSQLI_ASSOC);
         if ($pages_count <= 0) {
             $error = "Ничего не найдено по вашему запросу.";
             $page_content = include_template('error.php', ['error' => $error]);
         } else {
-            /*$page_content = include_template('_search.php', ['lots' => $lots, 'categories' => $categories,'pages' => $pages,
-            'pages_count' => $pages_count,
-            'cur_page' => $cur_page,
-            'link' => $link]);*/
             $page_content = include_template('_category.php', compact('lots', 'categories', 'pages', 'pages_count', 'cur_page', 'link'));
         }
     }
-
-
-
-
 
     $layout_content = include_template('layout.php', [
         'content'    => $page_content,
